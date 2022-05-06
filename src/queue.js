77f892422926a63ca6214ac0fb2bb2e935d31698
BB-pyps <80970001+BB-pyps@users.noreply.github.com>
2022-05-06 23:02:53 +0300

    feat: finish stack task

--- src/queue.js
+++ src/queue.js
@@ -14,7 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  constructor(){
+    this.head = null;
+    this.tail = null;
+  }
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
